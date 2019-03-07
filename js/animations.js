33f57acf66f408be0763e4a81f4cd1db8511a5f2
DaymLicuador <raulenhimalaya@yahoo.com>
2019-03-06 19:35:32 -0600

    test img

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
+
     $(".STUR").animate({
     opacity: '1'
     },200000);
-    
+
+    setTimeout(function(){
+
+        alert("tu mama es hombre");
+    },200000);
 });
